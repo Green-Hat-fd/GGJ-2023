b44a86fc8aa65988f5a920eda8eb9014c007cd20
Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
2023-02-05 14:19:41 +0100

    Tolto audio

--- Unity Proj/GGJ 2023 - Roots/Assets/Matin/Script/Player.cs
+++ Unity Proj/GGJ 2023 - Roots/Assets/Matin/Script/Player.cs
@@ -63,7 +63,7 @@ public class Player : MonoBehaviour
             GetComponent<SpriteRenderer>().enabled = false;
             GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
 
-            sfxMorte.Play();
+            //sfxMorte.Play();
 
             if(tempoTrascorsoDopoMorte >= tempoAspettareDopoMorte)
             {
@@ -86,7 +86,7 @@ public class Player : MonoBehaviour
             if ((Input.GetKeyDown(KeyCode.W) && groundedPlayer || Input.GetKeyDown(KeyCode.Space)) && groundedPlayer)
             {
                 controller.velocity = Vector2.up * jumpHeight;
-                sfxSalto.Play();
+                //sfxSalto.Play();
             }
 
             if (moveInput > 0) //Cambia la direzione dello sprite per il movimento laterale verso destra
